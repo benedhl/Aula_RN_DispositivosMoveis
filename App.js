5b543c726db0e6b9f16a692a3d66ed027f572b7e
Luiz Henrique <luizhenrique50@live.com>
2020-11-30 00:55:19 -0300

    Criando uma Função que retorna um componente

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Simples from './src/components/Simples';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Simples texto = 'Olá tudo bem?'/>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
