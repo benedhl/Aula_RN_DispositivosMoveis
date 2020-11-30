596d19d16c3c44ecd7bd1195876ff31521010d6a
Luiz Henrique <luizhenrique50@live.com>
2020-11-30 01:40:32 -0300

    Renderização Condicional

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// Nossos componentes
 import Simples from './src/components/Simples';
-
-
+import ParImpar from './src/components/ParImpar';
+// JSX
 export default function App() {
   return (
-    <Simples texto = 'Olá tudo bem?'/>
-
+    <View>
+      <Simples texto = 'Olá tudo bem?'/>
+      <ParImpar numero='11' />
+    </View>
   );
 }
 
